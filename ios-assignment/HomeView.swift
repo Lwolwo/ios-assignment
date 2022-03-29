9ce31188dcfbe57bb970d64f926a6f50fcb1b1e4
LuoPeijun <peijun.luo@thoughtworks.com>
2022-03-29 19:09:38 +0800

    feat: add TabView to create tabBar in bottom

---
+++ ios-assignment/HomeView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  ios-assignment
+//
+//  Created by Peijun Luo on 2022/3/29.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        TabView{
+            Text("微信页面")
+                .tabItem({
+                    Image(systemName: "message")
+                    Text("微信")
+                })
+            Text("通讯录页面")
+                .tabItem({
+                    Image(systemName: "phone")
+                    Text("通讯录")
+                })
+            Text("发现页面")
+                .tabItem({
+                    Image(systemName: "safari")
+                    Text("发现")
+                })
+            Text("我页面")
+                .tabItem({
+                    Image(systemName: "safari")
+                    Text("我")
+                })
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            HomeView()
+        }
+    }
+}
