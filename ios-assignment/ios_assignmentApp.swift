fab2f0fa1f992bc6ce39e3be32d5375e45c1604e
LuoPeijun <peijun.luo@thoughtworks.com>
2022-03-29 18:43:56 +0800

    Initial Commit

---
+++ ios-assignment/ios_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_assignmentApp.swift
+//  ios-assignment
+//
+//  Created by Peijun Luo on 2022/3/29.
+//
+
+import SwiftUI
+
+@main
+struct ios_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
